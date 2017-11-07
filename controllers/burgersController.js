5108f982d0fef89d9c2e9921a3a63ba007ef79e2
Daniel Muller <29293369+drmulr@users.noreply.github.com>
2017-11-06 23:00:32 -0500

    CSS Basic Layout

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -28,14 +28,12 @@ router.post("/api/burgers", function(req, res) {
 
 
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
+  burger.update({ eaten: req.body.eaten }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,19 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-eaten").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
-
-    var newSleepState = {
-      sleepy: newSleep
+    var newEaten = $(this).data("neweaten");
+    var newEatenState = {
+      eaten: "1"
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newEatenState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed eaten to", newEaten);
         // Reload the page to get the updated list
         location.reload();
       }
